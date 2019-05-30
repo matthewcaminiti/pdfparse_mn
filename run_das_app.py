09e30ef78ccd0269b8a715d4e65ea3048f682539
Matthew Caminiti <matthew.caminiti@mail.mcgill.ca>
2019-05-29 20:18:48 -0600

    gitignore

--- run_das_app.py
+++ run_das_app.py
@@ -1,11 +1,5 @@
 import sys
 import os
-# sys.path.append(os.getcwd() + "../pdf_parse")
-# sys.path.append(os.getcwd() + "../ui")
-# sys.path.append(os.getcwd() + "../web")
-# import processing
-# import concept_ui as ui
-# import drawing_dl as dl
 from pdf_parse.processing import *
 from ui.concept_ui import *
 from web.drawing_dl import *
