da077f705bda2c6621c84838398965c63af7592e
Matthew Caminiti <matthew.caminiti@mail.mcgill.ca>
2019-05-29 19:58:09 -0600

    Import cleaning and input/output pdf~~>excel flush completed

---
+++ run_das_app.py
@@ -0,0 +1,15 @@
+import sys
+import os
+# sys.path.append(os.getcwd() + "../pdf_parse")
+# sys.path.append(os.getcwd() + "../ui")
+# sys.path.append(os.getcwd() + "../web")
+# import processing
+# import concept_ui as ui
+# import drawing_dl as dl
+from pdf_parse.processing import *
+from ui.concept_ui import *
+from web.drawing_dl import *
+
+
+flush_input()
+
