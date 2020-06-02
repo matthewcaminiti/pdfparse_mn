88e0d653167ca8944bb97fe8a681b3bf4b83c79f
matthewcaminiti <matthew.caminiti@mail.mcgill.ca>
2020-06-01 22:11:08 -0400

    Gitignore adds for venv and removed unnecessary imports in main

--- run_das_app.py
+++ run_das_app.py
@@ -1,8 +1,8 @@
 import sys
 import os
 from pdf_parse.processing import *
-from ui.concept_ui import *
-from web.drawing_dl import *
+# from ui.concept_ui import *
+# from web.drawing_dl import *
 
 
 flush_input()
